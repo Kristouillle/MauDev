12ff51b7b1973b12e9204d572bd5c282abbd0630
Chris V <74806407+Kristouillle@users.noreply.github.com>
2026-05-01 15:30:29 -0400

    Change site URL and output type in astro config
    Updated site URL and output type in configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,8 @@
 import { defineConfig } from 'astro/config';
 import db from '@astrojs/db';
-import node from '@astrojs/node';
 
 export default defineConfig({
-  site: 'https://kshtech.ca/',
-  output: 'server',
-  security: {
-    allowedDomains: [{
-      hostname: 'kshtech.ca',
-      protocol: 'https'
-    }, {
-      hostname: '**.kshtech.ca',
-      protocol: 'https'
-    }, {
-      hostname: 'localhost',
-      protocol: 'http',
-      port: '4321'
-    }]
-  },
+  site: 'https://mau-dev.ca/',
+  output: 'static',
   integrations: [db()],
-  adapter: node({
-    mode: 'standalone',
-    staticHeaders: true,
-  }),
 });
